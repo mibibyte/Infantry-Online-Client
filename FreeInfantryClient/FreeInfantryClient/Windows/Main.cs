b14570b2e0eef951565e3c4719c940d72620f90d
Joshua <summerexit@hotmail.com>
2018-11-07 23:48:50 -0600

    Initial

---
+++ FreeInfantryClient/FreeInfantryClient/Windows/Main.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FreeInfantryClient.Windows
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
